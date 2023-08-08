b92eb8fd4b8aa55d496f6c8d0c4b2532e1e509ce
Sam <90459209+sam-trg@users.noreply.github.com>
2023-08-09 02:21:55 +0530

    Update .gitignore

--- src/exercises/Lab2QaQ2.java
+++ src/exercises/Lab2QaQ2.java
@@ -3,6 +3,7 @@ package exercises;
  * The first row has length 1, the second row has length 2, the third row has length 3, and so on. 
  * Then initialize the array using nested for loops so that the value of A[i][j] is i+j. 
  * Finally, print out the array in a nice triangular form*/
+
 public class Lab2QaQ2 {
 	
 	public static void main(String[] args) {
