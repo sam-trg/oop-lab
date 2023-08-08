98b67f3dc509e560e810f860a9ac6e74c717229d
Sam <90459209+sam-trg@users.noreply.github.com>
2023-08-09 03:11:43 +0530

    Test

--- src/exercises/Lab1aQ2.java
+++ src/exercises/Lab1aQ2.java
@@ -1,5 +1,5 @@
 package exercises;
-/* Write a method findSum to find the sum of digits of a number */
+/* Write a method findSum to find the sum of digits of a number  */
 import java.util.Scanner;
 
 public class Lab1aQ2 {
