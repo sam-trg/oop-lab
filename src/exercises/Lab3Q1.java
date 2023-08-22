68ae96853ec8db760674c1c66e990c7dd7a1c78d
Sam <90459209+sam-trg@users.noreply.github.com>
2023-08-22 14:24:24 +0530

    Add Lab3Q1

---
+++ src/exercises/Lab3Q1.java
@@ -0,0 +1,46 @@
+package exercises;
+
+public class Lab3Q1{
+	
+	public static void main(String[] args) {
+		
+		STUDENT s1 = new STUDENT();
+		int[] marks1 = {90,95,97,100};
+		s1.assign("Tarak Mehta", marks1);
+		s1.display();
+		
+		STUDENT s2 = new STUDENT();
+		int[] marks2 = {30,35,37,65};
+		s2.assign("Ishan Awasthi", marks2);
+		s2.display();
+	}
+}
+
+class STUDENT {
+	
+	private String sname;
+	private int[] marks_array;
+	private int total;
+	private double avg;
+		
+	public void assign(String name, int[] marks) {
+			
+		sname = name;
+		marks_array = marks;
+		compute();
+	}
+		
+	public void compute() {
+			
+		for(int marks:marks_array) {
+			total += marks;
+		}
+		avg = (float)total/marks_array.length;
+	}
+
+	public void display() {
+		
+		System.out.println("Student "+sname+" has "+total+" marks with an average of "+avg);
+	}
+		
+}
