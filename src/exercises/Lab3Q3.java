a6aa2e245731d7c00d70ea4d50fe9fb075f6ef01
Sam <90459209+sam-trg@users.noreply.github.com>
2023-08-22 15:22:20 +0530

    Boilerplate

---
+++ src/exercises/Lab3Q3.java
@@ -0,0 +1,35 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Lab3Q3 {
+
+	public static void main(String[] args) {
+		
+		//
+	}
+}
+
+class Mixer {
+	
+	private int arr[];
+	
+	public void accept() {
+		
+		//
+	}
+	
+	public Mixer mix(Mixer A) {
+		
+		//
+	
+		return //;
+	}
+	
+	public void display() {
+		
+		//
+	}
+}
+	
+	
